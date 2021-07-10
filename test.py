0ae2e366167680d2ddd5ae6d5f230bf355fa5b94
lihanxing <2645376975@qq.com>
2021-07-10 23:58:03 +0800

    修改前正确版本

--- test.py
+++ test.py
@@ -13,10 +13,13 @@ from layers import BilinearUpSampling2D
 from utils import predict, load_images, display_images
 from matplotlib import pyplot as plt
 
+
+
 # Argument Parser
 parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
 parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras model file.')
 parser.add_argument('--input', default='examples/*.png', type=str, help='Input filename or folder.')
+# parser.add_argument('--output',default='test_result', type=str, help='Output filename or folder.')
 args = parser.parse_args()
 
 # Custom object needed for inference and training
