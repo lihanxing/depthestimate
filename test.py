2bcdc3711e6dbd7e62e22052a12c22443fa36d14
lihanxing <2645376975@qq.com>
2021-07-30 17:41:59 +0800

    保存预测结果为单通道灰度图

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import time
 from PIL import Image
 import cv2
 import numpy as np
+import torchvision.transforms as transforms
 
 # Keras / TensorFlow
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
@@ -75,7 +76,24 @@ for i, j, k in os.walk(filePath):
 
     plt.savefig(("%s"%path)+"/depth.png",bbox_inches='tight',pad_inches = 0)
     plt.clf()
+    
+    # 图片路径，相对路径
+    image_path = path+"/depth.png"
+    # 读取图片
+    image = Image.open(image_path)
+    # 输出维度
+    print("RGB图像的维度：", np.array(image).shape)
+    # 显示原图
 
+    # RGB转换我灰度图像
+    image_transforms = transforms.Compose([
+        transforms.Grayscale(1)
+    ])
+    image = image_transforms(image)
+    # 输出灰度图像的维度
+    print("灰度图像维度： ", np.array(image).shape)
+    # 显示灰度图像
+    image.save(image_path)
 # # Input images
 # inputs = load_images( glob.glob(args.input) )
 # temp = Image.open(args.input)
